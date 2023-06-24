0cdbc38cfb79f6758b8ce5b5b81df02d76c965ad
thallyslemos <thallysvilemos@gmail.com>
2023-06-24 11:01:27 -0300

    create cards layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+
       },
+      colors:{
+        'BLUE-1':'#1d42cf',
+        'BLUE-2':'#3888fc'
+      }
     },
   },
   plugins: [],
